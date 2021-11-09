74e68a9edd8e625a81ffb98a964b28d58f8289d9
S M Faraz Shah <smfshah.bese19seecs@seecs.edu.pk>
2021-11-10 02:02:39 +0500

    UI created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
+import axios from 'axios'
 
 function App() {
+  const [url, seturl] = useState("");
+  const [shorturl, setshorturl] = useState("");
+
+  const onurlchange = (e) => {
+    seturl(e.target.value);
+  }
+  const submit = async () => {
+    const urlResult =  await axios({
+      url:`http://localhost:/6000/url/check`,//correct url
+      method:"post",
+      data:{
+          url:url,
+      }
+  });
+    console.log(urlResult);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen flex justify-center items-center bg-gray-100">
+      <div className=" w-1/2 bg-white shadow-xl self-center py-12 px-16 border-4 border-blue-500 rounded">
+        <h1 className="text-4xl my-4 text-blue-600 font-semibold text-center uppercase">URL Shortener</h1>
+        <div className="grid grid-cols-3 my-12">
+          <p className="text-gray-700 text-lg transform translate-y-1">Enter the url : </p>
+          <input className="col-span-2"  value={url} onChange={onurlchange} type="text" placeholder="www.yahoo.com"></input>
+        </div>
+        <p className="text-blue-600 italic text-lg my-6 text-center">shortendeaskdjfl.com</p>
+        <div className=" flex justify-center  my-12"> 
+            <button onClick={()=>{submit()}} className="w-24 h-12 text-lg rounded border-2 border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white">Submit</button>
+        </div>
+      </div>
     </div>
   );
 }
