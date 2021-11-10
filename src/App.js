044be45ddb8fce0ec79113b001e5872219d47cdd
S M Faraz Shah <smfshah.bese19seecs@seecs.edu.pk>
2021-11-10 10:34:56 +0500

    functionality completed

--- src/App.js
+++ src/App.js
@@ -4,30 +4,47 @@ import axios from 'axios'
 
 function App() {
   const [url, seturl] = useState("");
-  const [shorturl, setshorturl] = useState("");
+  const [slug, setslug] = useState("");
+  const [redirecturl, setredirecturl] = useState("");
+  const [message, setmessage] = useState("")
 
   const onurlchange = (e) => {
     seturl(e.target.value);
   }
   const submit = async () => {
     const urlResult =  await axios({
-      url:`http://localhost:/6000/url/check`,//correct url
+      url:`http://localhost:5000/url/check`,//correct url
       method:"post",
       data:{
           url:url,
       }
   });
     console.log(urlResult);
+    if(urlResult.data.url){
+      setredirecturl(urlResult.data.url);
+      setslug("");
+      setmessage("")
+      window.location.assign(urlResult.data.url)
+    }else if(urlResult.data.slug){
+      setslug(urlResult.data.slug);
+      setredirecturl("");
+      setmessage("");
+    }else if(urlResult.data.message){
+      setmessage(urlResult.data.message);
+      setslug("");
+      setredirecturl("");
+    }
   }
   return (
     <div className="h-screen flex justify-center items-center bg-gray-100">
-      <div className=" w-1/2 bg-white shadow-xl self-center py-12 px-16 border-4 border-blue-500 rounded">
+      <div className="w-10/12 lg:w-1/2 bg-white shadow-xl self-center py-12 px-16 border-4 border-blue-500 rounded">
         <h1 className="text-4xl my-4 text-blue-600 font-semibold text-center uppercase">URL Shortener</h1>
         <div className="grid grid-cols-3 my-12">
           <p className="text-gray-700 text-lg transform translate-y-1">Enter the url : </p>
-          <input className="col-span-2"  value={url} onChange={onurlchange} type="text" placeholder="www.yahoo.com"></input>
+          <input className="col-span-2"  value={url} onChange={onurlchange} type="text" placeholder="https://www.yahoo.com"></input>
         </div>
-        <p className="text-blue-600 italic text-lg my-6 text-center">shortendeaskdjfl.com</p>
+        <p className={` text-red-600 text-lg text-center `}>{message}</p>
+        <p className={`${redirecturl?"text-blue-800":""} text-blue-600 italic text-lg my-6 text-center`}>{redirecturl?`you are being redirected to '${redirecturl}'`:slug}</p>
         <div className=" flex justify-center  my-12"> 
             <button onClick={()=>{submit()}} className="w-24 h-12 text-lg rounded border-2 border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white">Submit</button>
         </div>
